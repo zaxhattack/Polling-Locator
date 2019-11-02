22439febd0005495d7cdb8264f355706d0a11d66
scott-wilkins <scott.wilkins@tamu.edu>
2019-11-02 11:34:36 -0500

    Add start command

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('static'));
+app.use(express.static(__dirname + '/static'));
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
