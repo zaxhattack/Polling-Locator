a3c13ef00378eecf6ec53da9d432239d320f2fd7
scott-wilkins <scott.wilkins@tamu.edu>
2019-11-11 16:23:19 -0600

    Add request methods to server

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
+const request = require('request');
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/static'));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/location", (req, res) => {
     console.log("Received GET on /location");
+});
+
+app.get("/representatives", (req, res) => {
+	console.log("Recieved GET on /representatives");
 });
 
 app.listen(PORT, () => {
