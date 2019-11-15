8af33fa031105e16ad96c3d85763dbc8cff9fbb2
scott-wilkins <scott.wilkins@tamu.edu>
2019-11-14 23:41:03 -0600

    Add sample data to test with

--- router.js
+++ router.js
@@ -1,9 +1,35 @@
 const express = require('express');
 const request = require('request');
+const collegeStation = require("./static/sample_data/717_University_Drive_TX.json");
+const countyDowns = require("./static/sample_data/327_County_Downs_Road_AL.json");
+const brooklineAve = require("./static/sample_data/801_South_Brookline_Avenue_OK.json");
+const arkansasStreet = require("./static/sample_data/1022_Arkansas_Street_OK.json");
+const eastStreet = require("./static/sample_data/1402_East_12th_Street_OK.json");
+const meredithDrive = require("./static/sample_data/3913_Meredith_Drive_AL.json");
 const router = express.Router();
+
 
 router.get("/locations", (req, res) => {
     console.log("Received GET on /locations");
+    if(req.query.address === "717 University Drive"){
+    	res.json(JSON.stringify(collegeStation));
+    	return;
+    }else if(req.query.address === "327 County Downs Road"){
+    	res.json(JSON.stringify(countyDowns));
+    	return;
+    }else if(req.query.address === "801 South Brookline Avenue"){
+    	res.json(JSON.stringify(brooklineAve));
+    	return;
+    }else if(req.query.address === "1022 Arkansas Street"){
+    	res.json(JSON.stringify(arkansasStreet));
+    	return;
+    }else if(req.query.address === "1402 East 12th Street"){
+    	res.json(JSON.stringify(eastStreet));
+    	return;
+    }else if(req.query.address === "3913 Meredith Drive"){
+    	res.json(JSON.stringify(meredithDrive));
+    	return;
+    }
     
     let query = encodeURI(`${req.query.address} ${req.query.city} ${req.query.state}`);
     request.get(`https://www.googleapis.com/civicinfo/v2/voterinfo?key=${process.env.CIVIC_KEY}&address=${query}`, (error, response, body) => {
